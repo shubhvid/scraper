8dd8b40307fcd9140db2920933b433554f5f5ef6
Shubham Vidyarthi <shubham@mlacochet410w7.corp.emc.com>
2013-02-18 16:28:26 -0500

    committing first version

---
+++ scraper.py
@@ -0,0 +1,137 @@
+import sys
+import os
+import re
+import json
+from Queue import Queue
+import threading
+import urllib2
+from bs4 import BeautifulSoup
+
+DEBUG = False
+rpmDict_lock = threading.Lock()
+urlQueue = Queue()  # already synchronized
+rpmDict = {}
+
+def debug(msg):
+    if DEBUG:
+        print "DEBUG: %s" % msg
+
+#class Scrape:
+#
+#def consumer():
+#    while True:
+#        print sum(q.get())
+#
+#def producer(data_source):
+#    for line in data_source:
+#        q.put( map(int, line.split()) )
+#
+#Thread(target=producer, args=[SOME_INPUT_FILE_OR_SOMETHING]).start()
+#
+#for i in range(10):
+#    Thread(target=consumer).start()
+
+class Worker (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+    def run(self):
+        while True:
+            try:
+                url = urlQueue.get()
+                urlf = None
+                try:
+                    urlf = urllib2.urlopen(url)
+                    page = urlf.read()
+                    
+                except urllib2.HTTPError, e:
+                    debug("%s: page %s cannot be loaded" % (self.name, url))
+                    continue
+                finally:
+                    if urlf:
+                        urlf.close()
+                soup = BeautifulSoup(page)
+                for link in soup.find_all('a'):
+                    href = link.get('href')
+                    if href.startswith('?') or href.startswith('/'): # parent directories should be ignored
+                        continue
+                    if href.endswith('/'):
+                        debug ("%s: %s" % (self.name, href))
+                        urlQueue.put(url + href)
+                    else:
+                        with rpmDict_lock:
+                            rpmDict[href] = url
+            finally:
+                urlQueue.task_done()
+
+        
+
+class Scraper:
+    def __init__(self, url=None, outfile=None, workerThreads = 20):
+        self.url = url
+        self.outfile = outfile
+        self.workerThreads = workerThreads
+    
+    def printUsage(self):
+        print "\nUSAGE: "
+        print "%s --help\n" % sys.argv[0]
+        print "%s -u <url> -o output_file\n" % sys.argv[0]
+        sys.exit(1)
+
+    def parseCommandOptions(self, args):
+        #--------------------------------------
+        #  Parse args[] by iterating over it
+        #--------------------------------------
+        
+        while args:
+            arg = args[0] # This arg
+            del args[0]
+            
+            try:
+                if arg[0] == '-' or arg == "/?":
+                    if arg.lower() in ['--url', '-u', '-url']:
+                        self.url = args[0].strip()
+                        if not self.url.endswith('/')
+                            self.url = self.url + '/'
+                        del args[0]
+                    
+                    elif arg.lower() in ["-o", "--outfile", "-outfile", "--out", "-out"]:
+                        self.outfile = args[0].strip()
+                        del args[0]
+                    
+                    elif arg.lower() in ["-h", "--help", "/?"]:
+                        printUsage()
+                    
+                    else:
+                        fatal("Invalid argument %s" % arg)
+            
+            except IndexError:
+                error("Missing command line options after %s" % arg)
+                printUsage()
+    
+    def main(self):
+        args = sys.argv[1:]
+        self.parseCommandOptions(args)
+        debug("URL: %s" % self.url)
+        debug("Output File: %s" % os.path.abspath(self.outfile))
+        for i in range(self.workerThreads):
+            wt = Worker()
+            wt.name = "worker%s" % i
+            wt.start()
+        urlQueue.put(self.url)
+        urlQueue.join()
+        print rpmDict
+        jsonoutput = json.dumps(rpmDict, sort_keys=True)
+        f = None
+        try:
+            f = open(self.outfile + ".json", 'w')
+            f.write(jsonoutput)
+        finally:
+            if f:
+                f.close()
+
+
+if __name__ == "__main__":
+    scraper = Scraper()
+    scraper.main()
